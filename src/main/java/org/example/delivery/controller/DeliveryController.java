05f29113ac590e7b23b1c3716c3a345ee1674e3d
Faina <fadoms@taltech.ee>
2025-03-22 20:33:19 +0200

    replaced sql with liquibase

--- src/main/java/org/example/delivery/controller/DeliveryController.java
+++ src/main/java/org/example/delivery/controller/DeliveryController.java
@@ -1,13 +1,12 @@
 package org.example.delivery.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.example.delivery.service.DeliveryService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,21 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("api")
 @RestController
-@Tag(name = "Delivery", description = "API for calculating delivery fees.")
+@Tag(name = "Delivery", description = "Operations related to deliveries")
 public class DeliveryController {
     private final DeliveryService deliveryService;
 
     @Operation(
-            summary = "Get delivery fee from database based on chosen vehicle type and city.",
-            description = "Retrieves a base fee for given combination of vehicle and city, calculates additional fees based on the latest weather data, and returns the total fee."
+            summary = "Get delivery fee from database based on chosen vehicle type and city",
+            description = "Retrieves a base fee for given combination of vehicle and city, calculates additional fees based on the latest weather data, and returns the total fee"
     )
-    @ApiResponse(responseCode = "200", description = "Fee has been calculated successfully.")
-    @ApiResponse(responseCode = "400", description = "Invalid input or forbidden vehicle usage.")
-
+    @ApiResponse(responseCode = "200", description = "Fee has been calculated successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid input or forbidden vehicle usage")
     @GetMapping("public/fee")
     public ResponseEntity<?> getDeliveryFee(
-            @RequestParam(value = "city") String city,
-            @RequestParam(value = "vehicleType") String type
+            @Parameter(name = "city", description = "City for which the fee needs to be calculated") @RequestParam(value = "city") String city,
+            @Parameter(name = "vehicleType", description = "Type of vehicle to calculate fees for") @RequestParam(value = "vehicleType") String type
     ) {
         try {
             double deliveryFee = deliveryService.calculateTotalFee(type, city);
--- src/main/java/org/example/delivery/controller/SchedulerController.java
+++ src/main/java/org/example/delivery/controller/SchedulerController.java
@@ -1,5 +1,8 @@
 package org.example.delivery.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.delivery.component.WeatherDataFetcher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/scheduler")
+@Tag(name = "Scheduler", description = "Operations for running scheduled tasks manually")
 public class SchedulerController {
 
     private final WeatherDataFetcher weatherDataFetcher;
@@ -16,6 +20,8 @@ public class SchedulerController {
     }
 
     @GetMapping("/run")
+    @Operation(summary = "Trigger task", description = "Triggers a task for fetching weather info")
+    @ApiResponse(responseCode = "200", description = "Successful execution")
     public String triggerTask() {
         weatherDataFetcher.fetchWeatherData();
         return "Scheduled task executed manually!";
